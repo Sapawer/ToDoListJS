2bbf93b63e8d30b5202863a6768b2910a611b2ac
NianSolo <solonians30@gmail.com>
2023-10-04 04:08:59 -0700

    Update JS

---
+++ JS/index.js
@@ -0,0 +1,13 @@
+
+const addButton = document.querySelector(".fa-arrow-circle-down");
+
+addButton.addEventListener("click", function(){
+const toDo = document.querySelector("#input").value; //todo text 
+const addElement = document.getElementById("stack"); //ul
+const addList = document.createElement("li"); //li
+    addList.innerText = toDo; 
+    addElement.append(Object.assign( addList,  { textContent : toDo}));
+    
+    console.log(toDo);        
+});
+
