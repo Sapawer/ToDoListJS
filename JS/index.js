3c00fae86c66456ab94412623efc6c984d042513
NianSolo <solonians30@gmail.com>
2023-10-04 05:17:23 -0700

    Update JS

--- JS/index.js
+++ JS/index.js
@@ -8,6 +8,23 @@ const addList = document.createElement("li"); //li
     addList.innerText = toDo; 
     addElement.append(Object.assign( addList,  { textContent : toDo}));
     
-    console.log(toDo);        
+  //  console.log(toDo);        
 });
+
+document.addEventListener("keypress", function (event){
+    
+   // console.log(event.key);
+    if ( event.key === "Enter"){
+        const toDo = document.querySelector("#input").value; //todo text 
+        const addElement = document.getElementById("stack"); //ul
+        const addList = document.createElement("li"); //li
+        addList.innerText = toDo; 
+        addElement.append(Object.assign( addList,  { textContent : toDo}));
+    
+    //    console.log(toDo);        
+
+    }
+    
+
+})
 
